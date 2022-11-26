edae5187572ee338064b6a43ab6e5bf4ef0a8efb
vikramkanzariya <84660089+vikramkanzariya@users.noreply.github.com>
2022-11-26 09:31:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -15,19 +15,4 @@ export default App
 
 
 
-// ---> This is Second App <---
-
-// // import "./App.css";
-// // import CovidData from "./CovidData";
-// import Covid from "./Covid";
-
-
-// function App() {
-// return <div className="App">
-// 	{/* <CovidData/> */}
-// 	<Covid/>
-// </div>;
-// }
-
-// export default App;
   
